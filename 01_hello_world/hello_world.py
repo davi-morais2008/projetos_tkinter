03ffed06ee457e8dd3fdaf0718602806a4612c89
Davi Morais <davi.a.morais@aluno.senai.br>
2025-08-16 15:26:24 -0300

    MOD: Mudei umas coisas

--- 01_hello_world/hello_world.py
+++ 01_hello_world/hello_world.py
@@ -1,61 +1,54 @@
 import tkinter as tk
 
-#CRIAR TELA E DEFINIR FORMATO E POSIÇÃO
+# Criar janela
 janela = tk.Tk()
 janela.geometry("800x600+100+50")
-
-#COR DE FUNDO DA TELA E TITULO
 janela.configure(bg="#4682B4")
 janela.title("Hello World!")
-
-#FOTO DA JANELA
 janela.iconbitmap("01_hello_world/java.ico")
-
-#IMPEDIR QUE O USUÁRIO REDIMENSIONE A JANELA
 janela.resizable(True, True)
 
-frame_centro = tk.Frame(janela, bg=janela["bg"])
-frame_centro.pack(expand=True)
+# Frame central para organizar tudo
+frame_central = tk.Frame(janela, bg=janela["bg"])
+frame_central.pack(expand=True)  # Mantém no centro
 
-
-#COLOCANDO TEXTOS NO JANELA
-label_titulo = tk.Label(janela, 
+# Título
+label_titulo = tk.Label(frame_central, 
                         text="Hello World", 
                         font=("Arial", 24), 
                         foreground="darkblue",
-                        bg=janela["bg"],
-                        )
-
+                        bg=janela["bg"])
 label_titulo.pack(pady=10)
 
-#FUNÇÃO DE MOSTRAR NOME
+# Função de mostrar nome
 def mostrar_nome():
     nome_digitado = campo_nome.get()
     label_resultado.config(text=f"Olá, {nome_digitado}!")
 
-#PERGUNTANDO NOME AO USUÁRIO
-label_nome = tk.Label(text="Digite seu nome abaixo:",
+# Pergunta ao usuário
+label_nome = tk.Label(frame_central,
+                      text="Digite seu nome abaixo:",
                       font=("Arial", 24),
                       foreground="darkblue",
                       bg=janela["bg"])
 label_nome.pack(pady=10)
 
-#CAMPO DE DIGITAR O NOME
-campo_nome = tk.Entry(janela)
-campo_nome.pack(pady=10)
+# Campo de entrada
+campo_nome = tk.Entry(frame_central, font=("Arial", 16))
+campo_nome.pack(pady=5)
 
-#BOTAO DE ENVIAR O NOME
-botao_enviar = tk.Button(janela, 
+# Botão
+botao_enviar = tk.Button(frame_central, 
                          text="Enviar", 
                          command=mostrar_nome,
                          font=("Arial", 16))
-botao_enviar.pack(pady=5)
+botao_enviar.pack(pady=10)
 
-#MOSTRAR O TEXTO COM O NOME DIGITADO
-label_resultado = tk.Label(janela, 
+# Resultado
+label_resultado = tk.Label(frame_central, 
                            text="", 
+                           font=("Arial", 16),
                            bg=janela["bg"])
-label_resultado.pack()
-
+label_resultado.pack(pady=10)
 
 janela.mainloop()
