81a44796b3ffeff08b3d209ded14dd985c0e8da7
Davi Morais <davi.a.morais@aluno.senai.br>
2025-08-29 14:58:23 -0300

    MOD: testes com a IA

--- 02_bot_gemini/testando_api_gemini.py
+++ 02_bot_gemini/testando_api_gemini.py
@@ -1,9 +1,9 @@
 from google import genai
 
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
-client = genai.Client(api_key="AIzaSyD5l_20H1M3gT7INQxSF75KQi3yH3h-ML0")
+client = genai.Client(api_key="AIzaSyCDPGBuvtBf69NqBnWD3u6dT3jO5N53H-E")
 
 response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Faça um texto sobre como eu, Davi, odeio o Fernando Fernandes Odilon"
+    model="gemini-2.5-flash", contents=""
 )
 print(response.text)
