fad11e31ef77b8bab574eb82fc8602bbd5f43d69
Davi Morais <davi.a.morais@aluno.senai.br>
2025-11-14 15:28:03 -0300

    MOD: Corrigi uns erros e finalizei o projeto Sortei com TTKBootstrap.

--- 08_prova_formativa/bcd.py
+++ 08_prova_formativa/bcd.py
@@ -2,6 +2,7 @@ import sqlite3
 
 class BancoDeDados:
     def __init__(self, usuario_arquivo="08_prova_formativa/sorteio.db"):
+        self.usuario_arquivo = usuario_arquivo
         self.con = sqlite3.connect(usuario_arquivo)
         self.cursor = self.con.cursor()
         self.create_table()
@@ -14,16 +15,27 @@ class BancoDeDados:
         )
         """)
         self.con.commit()
+        self.con.close()
 
     def add_moves(self, resultado):
+        self.con = sqlite3.connect(self.usuario_arquivo)
+        self.cursor = self.con.cursor()
         self.cursor.execute("INSERT INTO jogadas (resultados) VALUES (?)", (resultado,))
         self.con.commit()
+        self.con.close()
 
 
     def select_moves(self):
+        self.con = sqlite3.connect(self.usuario_arquivo)
+        self.cursor = self.con.cursor()
         self.cursor.execute("SELECT resultados FROM jogadas")
-        return self.cursor.fetchall()
+        dados = self.cursor.fetchall()
+        self.con.close()
+        return dados
     
     def clean_table(self):
+        self.con = sqlite3.connect(self.usuario_arquivo)
+        self.cursor = self.con.cursor()
         self.cursor.execute("DELETE FROM jogadas")
         self.con.commit()
+        self.con.close()
